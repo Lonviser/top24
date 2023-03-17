6bf62755a4654b540a0ad360ddbe677feec03bef
Lonviser <47rex47@mail.ru>
2023-03-17 11:45:06 +0300

    add sidebat and add bg for slides

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -5,3 +5,10 @@ console.log(1)
 console.log(1)
 console.log(2)
 console.log(3)
+
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,10 @@
 //= components/script.js
 //= components/script2.js
 //= components/script3.js
+
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
